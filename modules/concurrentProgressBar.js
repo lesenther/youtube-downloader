4d83595ef04cc711de1c8ba454559bb682e6d2e6
Logan Esenther <logan@cal.berkeley.edu>
2020-03-02 14:55:35 -0800

    Improved concurrent progress bar

--- modules/concurrentProgressBar.js
+++ modules/concurrentProgressBar.js
@@ -1,16 +1,16 @@
 const ProgressBar = require('progress');
 
 const concurrentProgressBar = (_ => {
-  let totalSize = null;
+  let totalSize = 0;
   let id = 0;
-  let bar = null;
+  let bar = false;
   let downloads = [];
 
-  function create(size) {
-    if (bar) {
-      bar.terminate();
-    }
-
+  /**
+   *
+   * @param {*} size
+   */
+  function add(size) {
     totalSize += size;
     id++;
 
@@ -20,35 +20,52 @@ const concurrentProgressBar = (_ => {
       percent: 0
     });
 
-    bar = new ProgressBar(`  downloading ${id} videos [:bar] :rate/bps :percent :etas`, {
-      complete: '=',
-      incomplete: ' ',
-      width: 20,
+    bar = new ProgressBar(`  downloading ${id} video${id>1?'s':''} [:bar] :rate/bps :percent :etas`, {
+      complete: '█',
+      incomplete: '░',
+      width: 40,
       total: totalSize
     });
+
+    update(id);
 
     return id;
   }
 
-  function update(id, percent) {
-    downloads.find(download => download.id === id).percent = percent;
+  /**
+   *
+   * @param {*} id
+   * @param {*} percent
+   */
+  function update(id, percent = 0) {
+    // Update the proper record
+    const record = downloads.find(object => object.id == id);
+
+    if (!record) {
+      throw new Error(`Record not found:  ${id}`);
+    }
+
+    record.percent = percent;
+
+    // Recalculate weighed total
     const partial = downloads
     .map(download => download.size * download.percent)
     .reduce((a, b) => a + b, 0);
+
+    // Update the progress bar
     bar.update(partial/totalSize);
+
+    return record;
   }
 
   return {
     add: size => {
-      const id = create(size);
+      const id = add(size);
 
       return percent => update(id, percent)
     },
     clear: _ => {
-      if (bar) {
-        bar.terminate();
-      }
-
+      bar = false;
       totalSize = 0;
       downloads = [];
       id = 0;
---
+++ tests/concurrentProgressBar.tests.js
@@ -0,0 +1,68 @@
+const assert = require('assert');
+const progress = require('../modules/concurrentProgressBar');
+
+describe('test multiprogress', _ => {
+
+  it('should do single download', done => {
+    const bar1 = progress.add(1000);
+
+    let percent1 = 0;
+
+    const int = setInterval(_ => {
+      percent1 += 0.1;
+      bar1(percent1);
+
+      if (percent1 >= 1) {
+        clearInterval(int);
+        progress.clear();
+        done();
+      }
+    }, 100);
+  });
+
+  it('should do dual downloads', done => {
+    const bar1 = progress.add(1000);
+    const bar2 = progress.add(2000);
+
+    let percent1 = 0, percent2 = 0;
+
+    const int = setInterval(_ => {
+      if (percent1 < 1) percent1 += 0.1;
+      if (percent2 < 1) percent2 += 0.05;
+
+      bar1(percent1);
+      bar2(percent2);
+
+      if (percent1 >= 1 && percent2 >= 1) {
+        clearInterval(int);
+        progress.clear();
+        done();
+      }
+    }, 50);
+  });
+
+  it('should do triple downloads', done => {
+    const bar1 = progress.add(1000);
+    const bar2 = progress.add(2000);
+    const bar3 = progress.add(4000);
+
+    let percent1 = 0, percent2 = 0, percent3 = 0;
+
+    const int = setInterval(_ => {
+      if (percent1 < 1) percent1 += 0.1;
+      if (percent2 < 1) percent2 += 0.05;
+      if (percent3 < 1) percent3 += 0.01;
+
+      bar1(percent1);
+      bar2(percent2);
+      bar3(percent3);
+
+      if (percent1 >= 1 && percent2 >= 1 && percent3 >= 1) {
+        clearInterval(int);
+        progress.clear();
+        done();
+      }
+    }, 10);
+  });
+
+});
